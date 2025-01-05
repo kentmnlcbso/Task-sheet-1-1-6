f5fa9b63e9995837ed1ccf6a7501d9c57ce9326d
Kenneth Emanuel Cabusao <119332036+kentmnlcbso@users.noreply.github.com>
2025-01-05 12:09:34 +0000

    Done adding output

--- Palindrome.java
+++ Palindrome.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        StringBuilder reversedString = new StringBuilder(input);
+        reversedString.reverse();
+
+        if (input.equals(reversedString.toString())) {
+            System.out.println("The input string is a palindrome.");
+        } else {
+            System.out.println("The input string is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
